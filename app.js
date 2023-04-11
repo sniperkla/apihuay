e60f0b3cff5bf65918bb26debc9fd5915d8beaf9
Your Name <you@example.com>
2023-04-11 21:47:41 +0700

    sss

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const app = express();
 const puppeteer = require("puppeteer");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-// const Schema = require("./schema");
+const mongoose = require("mongoose");
+const Schema = require("./schema");
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-// mongoose
-//   .connect("mongodb://147.50.227.164:27017/data", { useNewUrlParser: true })
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch((err) => console.error("Could not connect to MongoDB", err));
+mongoose
+  .connect("mongodb://147.50.227.164:27017/data", { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 app.get("/huay", async (req, res) => {
   // const client = new MongoClient("mongodb://147.50.227.164:27017/data", {
@@ -1226,30 +1227,18 @@ app.get("/huay", async (req, res) => {
     res.status(404).json("request limit reached");
     await browser.close();
   } finally {
-    // client.connect((err) => {
-    //   if (err) {
-    //     console.error(err);
-    //     return;
-    //   }
-
-    //   // Access the database object
-    //   const db = client.db("data");
-
-    //   // Perform database operations
-    //   db.collection("xxx").insertOne({ name: "sss" }, (err, result) => {
-    //     if (err) {
-    //       console.error(err);
-    //       return;
-    //     }
-    //     console.log(result);
-    //     client.close();
-    //   });
-    // });
-
-    res.status(200).send(alldata);
+    Schema.deleteMany()
+      .then((result) => {
+        Schema.insertMany(alldata);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  //  res.status(200).send(alldata);
     await browser.close(); // Close the browser instance
   }
 });
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
 });
